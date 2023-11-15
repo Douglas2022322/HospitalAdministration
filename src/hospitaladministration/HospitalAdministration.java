82facc1c070450bbe3d9219dc7732fc7d7910bc9
Douglas Vinicius Dierings <146199335+Douglas2022322@users.noreply.github.com>
2023-11-15 12:32:02 +0000

    Update HospitalAdministration.java

--- src/hospitaladministration/HospitalAdministration.java
+++ src/hospitaladministration/HospitalAdministration.java
@@ -15,6 +15,12 @@ public class HospitalAdministration {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Patient p1 = new Patient("Douglas", "15/11/23", "O+");
+        Patient p2 = new Patient("Evelin", "16/07/2001", "A+");
+        
+        System.out.println(p1.getPatientID());
+        System.out.println(p2.getPatientID());
     }
     
 }
